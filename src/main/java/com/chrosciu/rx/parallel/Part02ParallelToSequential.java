c5c55252cdbd5c8912a3955060df65f296651d07
Marcin Chrost <marcin.chrost@contractors.roche.com>
2020-09-29 20:34:35 +0200

    WIP

---
+++ src/main/java/com/chrosciu/rx/parallel/Part02ParallelToSequential.java
@@ -0,0 +1,23 @@
+package com.chrosciu.rx.parallel;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.ParallelFlux;
+import reactor.core.scheduler.Schedulers;
+
+@Slf4j
+public class Part02ParallelToSequential {
+    public static void main(String[] args) throws Exception {
+        Flux<Integer> flux = Flux.range(1, 10);
+        ParallelFlux<Integer> parallelFlux = flux.parallel()
+                .runOn(Schedulers.parallel())
+                .map(i -> i * 2)
+                .log();
+        Flux<Integer> mergedFlux = parallelFlux.sequential()
+                .map(i -> i + 1)
+                .log();
+
+        mergedFlux.subscribe(i -> log.info("Item: {}", i), e -> log.warn("", e), () -> log.info("completed"));
+        Thread.sleep(1000);
+    }
+}
