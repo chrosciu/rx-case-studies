fc86b55bcc044ab36c2900cdc3ddeeeddd6ae827
Marcin Chrost <mchrost82@gmail.com>
2022-10-25 08:05:42 +0200

    Sleep replaced with latch

--- src/main/java/com/chrosciu/rx/threads/Part04ReactiveFromBlocking.java
+++ src/main/java/com/chrosciu/rx/threads/Part04ReactiveFromBlocking.java
@@ -1,28 +1,33 @@
 package com.chrosciu.rx.threads;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 @Slf4j
 public class Part04ReactiveFromBlocking {
     public static void main(String[] args) throws Exception {
+        CountDownLatch latch = new CountDownLatch(1);
         Flux<String> logins = Flux.defer(() -> Flux.fromIterable(getUserLogins())).subscribeOn(Schedulers.boundedElastic());
+
         log.info("Before subscribe");
 
-        logins.subscribe(s -> log.info(s), e -> log.warn("", e));
+        logins.doFinally(st -> latch.countDown()).subscribe(s -> log.info(s), e -> log.warn("", e));
 
         log.info("After subscribe");
 
-        Thread.sleep(1000);
+        latch.await();
 
     }
 
+    @SneakyThrows
     private static List<String> getUserLogins() {
-        //blocks for long time
+        Thread.sleep(1000);
         log.info("Blocking !!!");
         return Arrays.asList("A", "B");
     }
--- src/main/java/com/chrosciu/rx/threads/Part05ReactiveToBlocking.java
+++ src/main/java/com/chrosciu/rx/threads/Part05ReactiveToBlocking.java
@@ -1,23 +1,28 @@
 package com.chrosciu.rx.threads;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
+
+import java.util.concurrent.CountDownLatch;
 
 @Slf4j
 public class Part05ReactiveToBlocking {
 
     public static void main(String[] args) throws Exception {
+        CountDownLatch latch = new CountDownLatch(1);
         Flux<String> logins = Flux.just("A", "B").publishOn(Schedulers.boundedElastic());
 
-        logins.doOnNext(u -> save(u)).subscribe(s -> log.info(s), e -> log.warn("", e));
+        logins.doFinally(st -> latch.countDown()).doOnNext(u -> save(u)).subscribe(s -> log.info(s), e -> log.warn("", e));
 
-
-        Thread.sleep(1000);
+        latch.await();
     }
 
+    @SneakyThrows
     private static void save(String login) {
         //blocks for long time
+        Thread.sleep(1000);
         log.info("Blocking save !!! for " + login);
     }
 }
