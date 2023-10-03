8b6268cabbbbdfcae8f9668791b24f61f7fd8189
Marcin Chrost <mchrost82@gmail.com>
2023-10-03 08:44:25 +0200

    Updated examples for Flux.create and Flux.generate

--- src/main/java/com/chrosciu/rx/flux/Part02FluxGenerate.java
+++ src/main/java/com/chrosciu/rx/flux/Part02FluxGenerate.java
@@ -1,22 +1,27 @@
 package com.chrosciu.rx.flux;
 
+import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Publisher;
+import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.SynchronousSink;
 
 import java.util.concurrent.Callable;
 import java.util.function.BiFunction;
 
+@Slf4j
 public class Part02FluxGenerate {
-    public static Publisher<Long> create(long to) {
+    public static Flux<Long> create(long to) {
         return Flux.generate(new Callable<Long>() {
             @Override
             public Long call() throws Exception {
+                log.info("In call");
                 return 1L;
             }
         }, new BiFunction<Long, SynchronousSink<Long>, Long>() {
             @Override
             public Long apply(Long context, SynchronousSink<Long> sink) {
+                log.info("In generator, context: {}", context);
                 sink.next(context);
                 if (context == to) {
                     sink.complete();
@@ -24,5 +29,22 @@ public class Part02FluxGenerate {
                 return context + 1;
             }
         });
+    }
+
+    public static void main(String[] args) {
+        log.info("Before create");
+
+        Flux<Long> flux = create(5L);
+
+        log.info("Before subscribe");
+
+        Disposable disposable = flux.subscribe(l -> log.info("Item: {}", l),
+                t -> log.info("Error: ", t),
+                () -> log.info("Completed"));
+
+        log.info("After subscribe");
+
+        log.info("{}", disposable.isDisposed());
+        disposable.dispose();
     }
 }
--- src/main/java/com/chrosciu/rx/flux/Part03FluxCreate.java
+++ src/main/java/com/chrosciu/rx/flux/Part03FluxCreate.java
@@ -2,6 +2,7 @@ package com.chrosciu.rx.flux;
 
 import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Publisher;
+import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
 
@@ -9,15 +10,34 @@ import java.util.function.Consumer;
 
 @Slf4j
 public class Part03FluxCreate {
-    public static Publisher<Long> create(long to) {
+    private static Flux<Long> create(long to) {
         return Flux.create(new Consumer<FluxSink<Long>>() {
             @Override
             public void accept(FluxSink<Long> sink) {
+                log.info("In sink");
                 for (long l = 1; l <= to; ++l) {
                     sink.next(l);
                 }
                 sink.complete();
             }
         });
+    }
+
+    public static void main(String[] args) {
+
+        log.info("Before create");
+
+        Flux<Long> flux = create(5L);
+
+        log.info("Before subscribe");
+
+        Disposable disposable = flux.subscribe(l -> log.info("Item: {}", l),
+                t -> log.info("Error: ", t),
+                () -> log.info("Completed"));
+
+        log.info("After subscribe");
+
+        log.info("{}", disposable.isDisposed());
+        disposable.dispose();
     }
 }
