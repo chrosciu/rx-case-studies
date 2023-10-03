552ae6342952af139b57f73c09627e25bd24dbbe
Marcin Chrost <mchrost82@gmail.com>
2023-10-03 16:32:30 +0200

    Test examples

--- src/test/java/com/chrosciu/rx/tests/Part01NaiveTest.java
+++ src/test/java/com/chrosciu/rx/tests/Part01NaiveTest.java
@@ -1,6 +1,5 @@
 package com.chrosciu.rx.tests;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 
@@ -8,23 +7,26 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Part01NaiveTest {
 
     @Test
     public void simpleFluxTest() {
         //given
-        Flux<String> flux = Flux.just("A", "B");
+        Flux<String> flux = simpleFlux();
 
         //when
         Stream<String> iterable = flux.toStream();
         List<String> list = iterable.collect(Collectors.toList());
 
         //then
-        Assertions.assertEquals(2, list.size());
-        Assertions.assertEquals("A", list.get(0));
-
-
-
+        assertEquals(2, list.size());
+        assertEquals("A", list.get(0));
+        assertEquals("B", list.get(0));
     }
 
+    private Flux<String> simpleFlux() {
+        return Flux.just("A", "B");
+    }
 }
