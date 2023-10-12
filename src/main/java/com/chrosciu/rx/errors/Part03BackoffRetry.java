4d794402862ff646d6a289bde9e8023d9cdc6863
Marcin Chrost <mchrost82@gmail.com>
2023-10-12 10:05:53 +0200

    Extended backoff example

--- src/main/java/com/chrosciu/rx/errors/Part03BackoffRetry.java
+++ src/main/java/com/chrosciu/rx/errors/Part03BackoffRetry.java
@@ -2,20 +2,40 @@ package com.chrosciu.rx.errors;
 
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.util.retry.Retry;
 
 import java.time.Duration;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 public class Part03BackoffRetry {
     public static void main(String[] args) throws Exception {
+        AtomicInteger counter = new AtomicInteger(0);
+        CountDownLatch latch = new CountDownLatch(1);
+
         Flux<String> original = Flux.just("A", "B").delayElements(Duration.ofMillis(50));
-        Flux<String> error = Flux.error(new RuntimeException("Baadf00d"));
+        Mono<String> error = Mono.fromCallable(() -> {
+            if (counter.getAndIncrement() < 2) {
+                throw new RuntimeException("Blah!");
+            }
+            return null;
+        });
         Flux<String> flux = Flux.concat(original, error);
 
-        Flux<String> withBackoff = flux.retryWhen(Retry.backoff(3, Duration.ofMillis(200)));
-        withBackoff.elapsed().subscribe(System.out::println, e -> log.warn("error:", e));
+        Flux<String> withBackoff = flux
+                .retryWhen(Retry.backoff(3, Duration.ofMillis(200)));
 
-        Thread.sleep(5000);
+        withBackoff
+                .elapsed()
+                .doFinally(st -> latch.countDown())
+                .subscribe(
+                i -> log.info("Item: {}", i),
+                e -> log.info("Error: ", e),
+                () -> log.info("Completed")
+        );
+
+        latch.await();
     }
 }
