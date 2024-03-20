552a2fd5f456dcd4fd54adae15f879805779b2f1
Marcin Chrost <mchrost82@gmail.com>
2024-03-20 19:43:33 +0100

    added example for when operator

---
+++ src/main/java/com/chrosciu/rx/operators/when/When.java
@@ -0,0 +1,33 @@
+package com.chrosciu.rx.operators.when;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@Slf4j
+class When {
+    @SneakyThrows
+    public static void main(String[] args) {
+        Flux<Long> flux1 = Flux.interval(Duration.ofMillis(300)).take(3)
+                .concatWith(Flux.error(new RuntimeException("Blah")))
+                .log("flux1");
+
+        Flux<String> flux2 = Flux.interval(Duration.ofMillis(200)).take(4)
+                .map(String::valueOf)
+                .log("flux2");
+
+        Mono<Void> mono = Mono.when(flux1, flux2);
+
+        mono.log("mono")
+                .subscribe(
+                        v -> log.info("Item: {}", v),
+                        t -> log.info("Error: ", t),
+                        () -> log.info("Completed")
+                );
+
+        Thread.sleep(1000);
+    }
+}
